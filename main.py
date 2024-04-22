d8fe5768c91cc4f6cf634bc5e2d32e81356a73f6
GOD OF THUNDER <93386525+yadhuhari@users.noreply.github.com>
2024-04-22 22:23:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,6 +38,85 @@ Mᴀɪɴᴛᴀɪɴᴇᴅ Bʏ: [ʜᴋᴢ ᴛɢ 🇮🇳](t.me/HKZTG)""",
             )
     )
 
+HELP_TEXT = """Hɪ {mention}. Wᴇʟᴄᴏᴍᴇ ᴛᴏ Sᴄʀᴇᴇɴsʜᴏᴛ Gᴇɴᴇʀᴀᴛᴏʀ Bᴏᴛ. Yᴏᴜ ᴄᴀɴ ᴜsᴇ ᴍᴇ ᴛᴏ ɢᴇɴᴇʀᴀᴛᴇ sᴄʀᴇᴇɴsʜᴏᴛs. 
+
+👉 I sᴜᴘᴘᴏʀᴛ ᴀɴʏ ᴋɪɴᴅ ᴏғ ᴛᴇʟᴇɢʀᴀᴍ ᴠɪᴅᴇᴏ ғɪʟᴇ (sᴛʀᴇᴀᴍɪɴɢ ᴠɪᴅᴇᴏ ᴏʀ ᴅᴏᴄᴜᴍᴇɴᴛ ᴠɪᴅᴇᴏ ғɪʟᴇs) ᴘʀᴏᴠɪᴅᴇᴅ ɪᴛ ʜᴀs ᴘʀᴏᴘᴇʀ ᴍɪᴍᴇ-ᴛʏᴘᴇ ᴀɴᴅ ɪs ɴᴏᴛ ᴄᴏʀʀᴜᴘᴛᴇᴅ.
+👉 I ᴀʟsᴏ sᴜᴘᴘᴏʀᴛ Sᴛʀᴇᴀᴍɪɴɢ URLs. Tʜᴇ URL sʜᴏᴜʟᴅ ʙᴇ ᴀ sᴛʀᴇᴀᴍɪɴɢ URL, ɴᴏɴ IP sᴘᴇᴄɪғɪᴄ, ᴀɴᴅ sʜᴏᴜʟᴅ ʀᴇᴛᴜʀɴ ᴘʀᴏᴘᴇʀ ʀᴇsᴘᴏɴsᴇ ᴄᴏᴅᴇs.
+Jᴜsᴛ sᴇɴᴅ ᴍᴇ ᴛʜᴇ ᴛᴇʟᴇɢʀᴀᴍ ғɪʟᴇ ᴏʀ ᴛʜᴇ sᴛʀᴇᴀᴍɪɴɢ URL.
+
+Sᴇᴇ /settings ᴛᴏ ᴄᴏɴғɪɢᴜʀᴇ ʙᴏᴛ's ʙᴇʜᴀᴠɪᴏʀ.
+Usᴇ /set_watermark ᴛᴏ sᴇᴛ ᴄᴜsᴛᴏᴍ ᴡᴀᴛᴇʀᴍᴀʀᴋs ᴛᴏ ʏᴏᴜʀ sᴄʀᴇᴇɴsʜᴏᴛs.
+
+Gᴇɴᴇʀᴀʟ FAQ.
+
+👉 Iғ ᴛʜᴇ ʙᴏᴛ ᴅᴏsᴇɴ'ᴛ ʀᴇsᴘᴏɴᴅ ᴛᴏ ᴛᴇʟᴇɢʀᴀᴍ ғɪʟᴇs ʏᴏᴜ ғᴏʀᴡᴀʀᴅ, ғɪʀsᴛ ᴄʜᴇᴄᴋ /sᴛᴀʀᴛ ᴀɴᴅ ᴄᴏɴғɪʀᴍ ʙᴏᴛ ɪs ᴀʟɪᴠᴇ. Tʜᴇɴ ᴍᴀᴋᴇ sᴜʀᴇ ᴛʜᴇ ғɪʟᴇ ɪs ᴀ ᴠɪᴅᴇᴏ ғɪʟᴇ ᴡʜɪᴄʜ sᴀᴛɪsғɪᴇs ᴀʙᴏᴠᴇ ᴍᴇɴᴛɪᴏɴᴇᴅ ᴄᴏɴᴅɪᴛɪᴏɴs.
+👉 Iғ ʙᴏᴛ ʀᴇᴘʟɪᴇs 😟 Sᴏʀʀʏ! I ᴄᴀɴɴᴏᴛ ᴏᴘᴇɴ ᴛʜᴇ ғɪʟᴇ., ᴛʜᴇ ғɪʟᴇ ᴍɪɢʜᴛ ʙᴇ ᴄᴜʀʀᴜᴘᴛᴇᴅ ᴏʀ ɪs ᴍᴀʟғᴏʀᴍᴀᴛᴛᴇᴅ.
+
+Iғ ɪssᴜᴇs ᴘᴇʀsɪsᴛs ᴄᴏɴᴛᴀᴄᴛ ᴍʏ Dᴇᴠᴇʟᴏᴘᴇʀ.
+"""
+
+@HKZ.on_message(filters.command("help"))
+async def help_cmd(client, message):
+    await message.reply_photo(
+        photo=random.choice(PICS), 
+        caption=f"""Hɪ {mention}. Wᴇʟᴄᴏᴍᴇ ᴛᴏ Sᴄʀᴇᴇɴsʜᴏᴛ Gᴇɴᴇʀᴀᴛᴏʀ Bᴏᴛ. Yᴏᴜ ᴄᴀɴ ᴜsᴇ ᴍᴇ ᴛᴏ ɢᴇɴᴇʀᴀᴛᴇ sᴄʀᴇᴇɴsʜᴏᴛs. 
+
+👉 I sᴜᴘᴘᴏʀᴛ ᴀɴʏ ᴋɪɴᴅ ᴏғ ᴛᴇʟᴇɢʀᴀᴍ ᴠɪᴅᴇᴏ ғɪʟᴇ (sᴛʀᴇᴀᴍɪɴɢ ᴠɪᴅᴇᴏ ᴏʀ ᴅᴏᴄᴜᴍᴇɴᴛ ᴠɪᴅᴇᴏ ғɪʟᴇs) ᴘʀᴏᴠɪᴅᴇᴅ ɪᴛ ʜᴀs ᴘʀᴏᴘᴇʀ ᴍɪᴍᴇ-ᴛʏᴘᴇ ᴀɴᴅ ɪs ɴᴏᴛ ᴄᴏʀʀᴜᴘᴛᴇᴅ.
+👉 I ᴀʟsᴏ sᴜᴘᴘᴏʀᴛ Sᴛʀᴇᴀᴍɪɴɢ URLs. Tʜᴇ URL sʜᴏᴜʟᴅ ʙᴇ ᴀ sᴛʀᴇᴀᴍɪɴɢ URL, ɴᴏɴ IP sᴘᴇᴄɪғɪᴄ, ᴀɴᴅ sʜᴏᴜʟᴅ ʀᴇᴛᴜʀɴ ᴘʀᴏᴘᴇʀ ʀᴇsᴘᴏɴsᴇ ᴄᴏᴅᴇs.
+Jᴜsᴛ sᴇɴᴅ ᴍᴇ ᴛʜᴇ ᴛᴇʟᴇɢʀᴀᴍ ғɪʟᴇ ᴏʀ ᴛʜᴇ sᴛʀᴇᴀᴍɪɴɢ URL.
+
+Sᴇᴇ /settings ᴛᴏ ᴄᴏɴғɪɢᴜʀᴇ ʙᴏᴛ's ʙᴇʜᴀᴠɪᴏʀ.
+Usᴇ /set_watermark ᴛᴏ sᴇᴛ ᴄᴜsᴛᴏᴍ ᴡᴀᴛᴇʀᴍᴀʀᴋs ᴛᴏ ʏᴏᴜʀ sᴄʀᴇᴇɴsʜᴏᴛs.
+
+Gᴇɴᴇʀᴀʟ FAQ.
+
+👉 Iғ ᴛʜᴇ ʙᴏᴛ ᴅᴏsᴇɴ'ᴛ ʀᴇsᴘᴏɴᴅ ᴛᴏ ᴛᴇʟᴇɢʀᴀᴍ ғɪʟᴇs ʏᴏᴜ ғᴏʀᴡᴀʀᴅ, ғɪʀsᴛ ᴄʜᴇᴄᴋ /sᴛᴀʀᴛ ᴀɴᴅ ᴄᴏɴғɪʀᴍ ʙᴏᴛ ɪs ᴀʟɪᴠᴇ. Tʜᴇɴ ᴍᴀᴋᴇ sᴜʀᴇ ᴛʜᴇ ғɪʟᴇ ɪs ᴀ ᴠɪᴅᴇᴏ ғɪʟᴇ ᴡʜɪᴄʜ sᴀᴛɪsғɪᴇs ᴀʙᴏᴠᴇ ᴍᴇɴᴛɪᴏɴᴇᴅ ᴄᴏɴᴅɪᴛɪᴏɴs.
+👉 Iғ ʙᴏᴛ ʀᴇᴘʟɪᴇs 😟 Sᴏʀʀʏ! I ᴄᴀɴɴᴏᴛ ᴏᴘᴇɴ ᴛʜᴇ ғɪʟᴇ., ᴛʜᴇ ғɪʟᴇ ᴍɪɢʜᴛ ʙᴇ ᴄᴜʀʀᴜᴘᴛᴇᴅ ᴏʀ ɪs ᴍᴀʟғᴏʀᴍᴀᴛᴛᴇᴅ.
+
+Iғ ɪssᴜᴇs ᴘᴇʀsɪsᴛs ᴄᴏɴᴛᴀᴄᴛ ᴍʏ Dᴇᴠᴇʟᴏᴘᴇʀ.
+""",
+        reply_markup=InlineKeyboardMarkup( [[
+            InlineKeyboardButton("Hᴇʟᴘ 🛠", callback_data="help"),
+            InlineKeyboardButton("Sᴇᴛᴛɪɴɢs ⚙", callback_data="set+settings")
+            ],[
+            InlineKeyboardButton("Cʟᴏsᴇ 📛", callback_data="close")
+            ]] 
+            )
+    )
+
+@HKZ.on_callback_query(
+    filters.create(lambda _, __, query: query.data.startswith("help"))
+)
+async def help_cb(c, m):
+    await message.answer()
+    await message.message.edit(
+        photo=random.choice(PICS), 
+        caption=f"""Hɪ {mention}. Wᴇʟᴄᴏᴍᴇ ᴛᴏ Sᴄʀᴇᴇɴsʜᴏᴛ Gᴇɴᴇʀᴀᴛᴏʀ Bᴏᴛ. Yᴏᴜ ᴄᴀɴ ᴜsᴇ ᴍᴇ ᴛᴏ ɢᴇɴᴇʀᴀᴛᴇ sᴄʀᴇᴇɴsʜᴏᴛs. 
+
+👉 I sᴜᴘᴘᴏʀᴛ ᴀɴʏ ᴋɪɴᴅ ᴏғ ᴛᴇʟᴇɢʀᴀᴍ ᴠɪᴅᴇᴏ ғɪʟᴇ (sᴛʀᴇᴀᴍɪɴɢ ᴠɪᴅᴇᴏ ᴏʀ ᴅᴏᴄᴜᴍᴇɴᴛ ᴠɪᴅᴇᴏ ғɪʟᴇs) ᴘʀᴏᴠɪᴅᴇᴅ ɪᴛ ʜᴀs ᴘʀᴏᴘᴇʀ ᴍɪᴍᴇ-ᴛʏᴘᴇ ᴀɴᴅ ɪs ɴᴏᴛ ᴄᴏʀʀᴜᴘᴛᴇᴅ.
+👉 I ᴀʟsᴏ sᴜᴘᴘᴏʀᴛ Sᴛʀᴇᴀᴍɪɴɢ URLs. Tʜᴇ URL sʜᴏᴜʟᴅ ʙᴇ ᴀ sᴛʀᴇᴀᴍɪɴɢ URL, ɴᴏɴ IP sᴘᴇᴄɪғɪᴄ, ᴀɴᴅ sʜᴏᴜʟᴅ ʀᴇᴛᴜʀɴ ᴘʀᴏᴘᴇʀ ʀᴇsᴘᴏɴsᴇ ᴄᴏᴅᴇs.
+Jᴜsᴛ sᴇɴᴅ ᴍᴇ ᴛʜᴇ ᴛᴇʟᴇɢʀᴀᴍ ғɪʟᴇ ᴏʀ ᴛʜᴇ sᴛʀᴇᴀᴍɪɴɢ URL.
+
+Sᴇᴇ /settings ᴛᴏ ᴄᴏɴғɪɢᴜʀᴇ ʙᴏᴛ's ʙᴇʜᴀᴠɪᴏʀ.
+Usᴇ /set_watermark ᴛᴏ sᴇᴛ ᴄᴜsᴛᴏᴍ ᴡᴀᴛᴇʀᴍᴀʀᴋs ᴛᴏ ʏᴏᴜʀ sᴄʀᴇᴇɴsʜᴏᴛs.
+
+Gᴇɴᴇʀᴀʟ FAQ.
+
+👉 Iғ ᴛʜᴇ ʙᴏᴛ ᴅᴏsᴇɴ'ᴛ ʀᴇsᴘᴏɴᴅ ᴛᴏ ᴛᴇʟᴇɢʀᴀᴍ ғɪʟᴇs ʏᴏᴜ ғᴏʀᴡᴀʀᴅ, ғɪʀsᴛ ᴄʜᴇᴄᴋ /sᴛᴀʀᴛ ᴀɴᴅ ᴄᴏɴғɪʀᴍ ʙᴏᴛ ɪs ᴀʟɪᴠᴇ. Tʜᴇɴ ᴍᴀᴋᴇ sᴜʀᴇ ᴛʜᴇ ғɪʟᴇ ɪs ᴀ ᴠɪᴅᴇᴏ ғɪʟᴇ ᴡʜɪᴄʜ sᴀᴛɪsғɪᴇs ᴀʙᴏᴠᴇ ᴍᴇɴᴛɪᴏɴᴇᴅ ᴄᴏɴᴅɪᴛɪᴏɴs.
+👉 Iғ ʙᴏᴛ ʀᴇᴘʟɪᴇs 😟 Sᴏʀʀʏ! I ᴄᴀɴɴᴏᴛ ᴏᴘᴇɴ ᴛʜᴇ ғɪʟᴇ., ᴛʜᴇ ғɪʟᴇ ᴍɪɢʜᴛ ʙᴇ ᴄᴜʀʀᴜᴘᴛᴇᴅ ᴏʀ ɪs ᴍᴀʟғᴏʀᴍᴀᴛᴛᴇᴅ.
+
+Iғ ɪssᴜᴇs ᴘᴇʀsɪsᴛs ᴄᴏɴᴛᴀᴄᴛ ᴍʏ Dᴇᴠᴇʟᴏᴘᴇʀ.
+""",
+        reply_markup=InlineKeyboardMarkup( [[
+            InlineKeyboardButton("Hᴇʟᴘ 🛠", callback_data="help"),
+            InlineKeyboardButton("Sᴇᴛᴛɪɴɢs ⚙", callback_data="set+settings")
+            ],[
+            InlineKeyboardButton("Cʟᴏsᴇ 📛", callback_data="close")
+            ]] 
+            )
+    
+
+
 @HKZ.on_callback_query(
     filters.create(lambda _, __, query: query.data.startswith("home"))
 )
