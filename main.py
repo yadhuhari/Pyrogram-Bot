e244e68abd45d990d68325a37404406323e564e7
GOD OF THUNDER <93386525+yadhuhari@users.noreply.github.com>
2024-04-22 20:09:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ HKZ = Client(
     api_id="23050566",
     api_hash="25e954ccd4afb778eea69bd6754275ff",
     bot_token="7037015366:AAGsTNCQ4lwm_dcSuWauRrk6MWsnKRMzNtI"
-   )
+)
 
 @HKZ.on_message(filters.command("start"))
 async def start_cmd(client, message):
