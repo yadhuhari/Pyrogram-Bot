a47a30bad4ed4bc61b30893eaaa3e5d2421a002e
GOD OF THUNDER <93386525+yadhuhari@users.noreply.github.com>
2024-04-22 20:06:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+from pyrogram import Client, filters
 
+HKZ = Client(
+    name="PyrogramBot",
+    api_id="23050566",
+    api_hash="25e954ccd4afb778eea69bd6754275ff",
+    bot_token="7037015366:AAGsTNCQ4lwm_dcSuWauRrk6MWsnKRMzNtI"
+   )
+
+@HKZ.on_message(filters.command("start"))
+async def start_cmd(client, message):
+    await message.reply_text("Hello Sir")
+
+print("Bot is running 🏃")
+
+HKZ.run()
